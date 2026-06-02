cf75b2bf58d5be07447425c2ccb698ff06d537a5
gazitaki <gazitakijewerly@gmail.com>
2026-06-03 00:13:56 +0300

    Add service worker with caching and push notifications

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'gazitaki-v1';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('push', function(event) {
+  if (!event.data) return;
+  var data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: 'https://i.ibb.co/chKqs2Qz/file-000000005a7c72438ba1cefaa8903576.png',
+      badge: 'https://i.ibb.co/chKqs2Qz/file-000000005a7c72438ba1cefaa8903576.png'
+    })
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
